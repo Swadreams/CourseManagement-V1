3329a209874d21574c25fa261d5c733f3502cff6
schute <schute@mobiquityinc.com>
2019-03-10 16:16:19 +0530

    Welcome Component, Routing concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list/course-list.component';
 import { StarComponent } from './shared/star.component';
+import { HeaderComponent } from './header/header.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    HeaderComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    //AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent },
+      {path: 'courses', component: CourseListComponent}, 
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'},
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -9,7 +9,7 @@ import { ICourse } from './course';
   providedIn: 'root'
 })
 export class CourseService {
-  url : string = "https://1raw.githubusercontent.com/Swadreams/Angular-Batch-Project/master/src/api/courses/courses.json";;
+  url : string = "https://raw.githubusercontent.com/Swadreams/Angular-Batch-Project/master/src/api/courses/courses.json";;
 
   constructor(private http: HttpClient) {
   }
