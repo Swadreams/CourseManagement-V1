aab5633fb739c4fd6c6525c704b6da8eef45e0ea
Swapnil <swadreams@gmail.com>
2019-03-10 08:15:21 +0530

    Course Service

---
+++ src/app/courses/course.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { ICourse } from './course';
+//import { ICourse } from './course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  url : string = "https://1raw.githubusercontent.com/Swadreams/Angular-Batch-Project/master/src/api/courses/courses.json";;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getCourses(): Observable<ICourse[]>  {
+    return this.http.get(this.url);
+              //  .pipe(
+              //    tap(response => console.log(response)),
+              //    catchError(this.errorHandling)
+              //  )
+  }
+
+  // errorHandling(err) {
+  //   let errorMessage = "";  
+  //   if(err.error instanceof ErrorEvent) {
+  //     errorMessage = `An error occurred ${err.error.message}`;
+  //   } else {
+  //     errorMessage = `Server returned code ${err.status}, error message is : ${err.message}`;
+  //   }
+  //   console.log(errorMessage);
+  //   return err;
+  // }
+}
